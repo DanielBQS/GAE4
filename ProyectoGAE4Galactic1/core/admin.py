c377df7b694e0adda28e6400b63b31047b3f548a
DanielBQS <135709348+DanielBQS@users.noreply.github.com>
2023-08-28 19:05:14 -0500

    Se agrega Modelos_Admin

--- ProyectoGAE4Galactic1/core/admin.py
+++ ProyectoGAE4Galactic1/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cliente, Proveedor, Rol, TipoProducto, Empleado, PQRS, PQRSRespuesta, Producto, Inventario, Venta, DetalleVenta
 
-# Register your models here.
+# Registrar los modelos en el panel de administración
+admin.site.register(Cliente)
+admin.site.register(Proveedor)
+admin.site.register(Rol)
+admin.site.register(TipoProducto)
+admin.site.register(Empleado)
+admin.site.register(PQRS)
+admin.site.register(PQRSRespuesta)
+admin.site.register(Producto)
+admin.site.register(Inventario)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
--- ProyectoGAE4Galactic1/core/models.py
+++ ProyectoGAE4Galactic1/core/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from datetime import datetime
 
+class Rol(models.Model):
+    nombre_rol = models.CharField(max_length=20, verbose_name='Rol')
+
+    def __str__(self):
+        return self.nombre_rol
+
+    class Meta:
+        verbose_name = 'Rol'
+        verbose_name_plural = 'Roles'
+        db_table = 'rol'
+        ordering = ['nombre_rol']
+
 class Cliente(models.Model):
     nombre_cliente = models.CharField(max_length=60, verbose_name='Nombre del cliente')
     Fecha_nacimiento = models.DateField(verbose_name='Fecha de nacimiento')
@@ -19,6 +31,57 @@ class Cliente(models.Model):
         db_table = 'cliente'
         ordering = ['nombre_cliente']
 
+class Empleado(models.Model):
+    nombre_empleado = models.CharField(max_length=60, verbose_name='Nombre del empleado')
+    CorreoElectronicoE = models.CharField(max_length=40, verbose_name='Correo electrónico')
+    DiereccionE = models.CharField(max_length=40, verbose_name='Dirección')
+    Num_identificacion = models.BigIntegerField(verbose_name='Número de identificación')
+    NombreUsuarioE = models.CharField(max_length=30, verbose_name='Nombre de usuario')
+    ContraseñaE = models.CharField(max_length=20, verbose_name='Contraseña')
+    Estado_Empleado = models.CharField(max_length=20, verbose_name='Estado del empleado')
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, verbose_name='Rol')
+
+    def __str__(self):
+        return self.nombre_empleado
+
+    class Meta:
+        verbose_name = 'Empleado'
+        verbose_name_plural = 'Empleados'
+        db_table = 'empleado'
+        ordering = ['nombre_empleado']
+
+class PQRS(models.Model):
+    tipoPQRS = models.CharField(max_length=20, verbose_name='Tipo de PQRS')
+    fechaPQRS = models.DateField(verbose_name='Fecha de PQRS')
+    EstadoPQRS = models.CharField(max_length=30, verbose_name='Estado de PQRS')
+    DescripcionPQRS = models.CharField(max_length=100, verbose_name='Descripción de PQRS')
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
+
+    def __str__(self):
+        return self.tipoPQRS
+
+    class Meta:
+        verbose_name = 'PQRS'
+        verbose_name_plural = 'PQRSs'
+        db_table = 'pqrs'
+        ordering = ['tipoPQRS']
+
+class PQRSRespuesta(models.Model):
+    respuesta = models.CharField(max_length=100, verbose_name='Respuesta')
+    estado = models.CharField(max_length=20, verbose_name='Estado de respuesta')
+    fecha_respuesta = models.DateField(verbose_name='Fecha de respuesta')
+    pqrs = models.ForeignKey(PQRS, on_delete=models.CASCADE, verbose_name='PQRS')
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE, verbose_name='Empleado')
+
+    def __str__(self):
+        return self.respuesta
+
+    class Meta:
+        verbose_name = 'Respuesta de PQRS'
+        verbose_name_plural = 'Respuestas de PQRS'
+        db_table = 'pqrs_respuesta'
+        ordering = ['respuesta']
+
 class Proveedor(models.Model):
     nombre_proveedor = models.CharField(max_length=60, verbose_name='Nombre del proveedor')
     TelefonoProveedor = models.BigIntegerField(verbose_name='Teléfono del proveedor')
@@ -35,26 +98,6 @@ class Proveedor(models.Model):
         db_table = 'proveedor'
         ordering = ['nombre_proveedor']
 
-class Rol(models.Model):
-    ADMINISTRADOR = 'Administrador'
-    EMPLEADO = 'Empleado'
-
-    ROL_CHOICES = [
-        (ADMINISTRADOR, 'Administrador'),
-        (EMPLEADO, 'Empleado'),
-    ]
-
-    nombre_rol = models.CharField(max_length=20, choices=ROL_CHOICES, verbose_name='Rol')
-
-    def __str__(self):
-        return self.nombre_rol
-
-    class Meta:
-        verbose_name = 'Rol'
-        verbose_name_plural = 'Roles'
-        db_table = 'rol'
-        ordering = ['nombre_rol']
-
 class TipoProducto(models.Model):
     nombre_Producto = models.CharField(max_length=20, verbose_name='Nombre del producto')
 
@@ -67,59 +110,13 @@ class TipoProducto(models.Model):
         db_table = 'tipo_producto'
         ordering = ['nombre_Producto']
 
-class Empleado(models.Model):
-    nombre_empleado = models.CharField(max_length=60, verbose_name='Nombre del empleado')
-    CorreoElectronicoE = models.CharField(max_length=40, verbose_name='Correo electrónico')
-    DiereccionE = models.CharField(max_length=40, verbose_name='Dirección')
-    Num_identificacion = models.BigIntegerField(verbose_name='Número de identificación')
-    NombreUsuarioE = models.CharField(max_length=30, verbose_name='Nombre de usuario')
-    ContraseñaE = models.CharField(max_length=20, verbose_name='Contraseña')
-    Estado_Empleado = models.CharField(max_length=20, verbose_name='Estado del empleado')
-
-    def __str__(self):
-        return self.nombre_empleado
-
-    class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
-        ordering = ['nombre_empleado']
-
-class PQRS(models.Model):
-    tipoPQRS = models.CharField(max_length=20, verbose_name='Tipo de PQRS')
-    fechaPQRS = models.DateField(verbose_name='Fecha de PQRS')
-    EstadoPQRS = models.CharField(max_length=30, verbose_name='Estado de PQRS')
-    DescripcionPQRS = models.CharField(max_length=100, verbose_name='Descripción de PQRS')
-
-    def __str__(self):
-        return self.tipoPQRS
-
-    class Meta:
-        verbose_name = 'PQRS'
-        verbose_name_plural = 'PQRSs'
-        db_table = 'pqrs'
-        ordering = ['tipoPQRS']
-
-class PQRSRespuesta(models.Model):
-    respuesta = models.CharField(max_length=100, verbose_name='Respuesta')
-    estado = models.CharField(max_length=20, verbose_name='Estado de respuesta')
-    fecha_respuesta = models.DateField(verbose_name='Fecha de respuesta')
-
-    def __str__(self):
-        return self.respuesta
-
-    class Meta:
-        verbose_name = 'Respuesta de PQRS'
-        verbose_name_plural = 'Respuestas de PQRS'
-        db_table = 'pqrs_respuesta'
-        ordering = ['respuesta']
-
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=60, verbose_name='Nombre del producto')
     Descripcion_Producto = models.CharField(max_length=60, verbose_name='Descripción del producto')
     PrecioProducto = models.BigIntegerField(verbose_name='Precio del producto')
     MarcaProducto = models.CharField(max_length=20, verbose_name='Marca del producto')
     Estado_Producto = models.CharField(max_length=20, verbose_name='Estado del producto')
+    tipo_producto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE, verbose_name='Tipo de producto')
 
     def __str__(self):
         return self.nombre_producto
@@ -132,7 +129,8 @@ class Producto(models.Model):
 
 class Inventario(models.Model):
     Cantidad_productos = models.IntegerField(verbose_name='Cantidad de productos')
-
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='Producto')
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, verbose_name='Proveedor del inventario')
     def __str__(self):
         return f'Inventario {self.id}'
 
@@ -147,9 +145,8 @@ class Venta(models.Model):
     Fecha_venta = models.DateTimeField(verbose_name='Fecha de venta')
     estado_venta = models.CharField(max_length=20, verbose_name='Estado de venta')
     email = models.CharField(max_length=40, verbose_name='Correo electrónico')
-    id_cliente = models.CharField(max_length=20, verbose_name='ID de cliente')
     total_venta = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Total de venta')
-
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente de la venta')
     def __str__(self):
         return f'Venta {self.id}'
 
@@ -163,8 +160,8 @@ class DetalleVenta(models.Model):
     NomVenta = models.CharField(max_length=30, verbose_name='Nombre de venta')
     PrecioVenta = models.BigIntegerField(verbose_name='Precio de venta')
     Cantidad = models.IntegerField(verbose_name='Cantidad')
-    id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE, verbose_name='ID de venta')
-    id_Eproducto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='ID de producto')
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE, verbose_name='Venta')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name='Producto')
 
     def __str__(self):
         return f'DetalleVenta {self.id}'
