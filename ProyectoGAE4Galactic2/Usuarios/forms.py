dff9b82e58454c8068cc52280c8271839445d283
DanielBQS <135709348+DanielBQS@users.noreply.github.com>
2024-03-10 16:53:05 -0500

    Diseño Final
    se agrego todas las validaciones de campos en el register

--- ProyectoGAE4Galactic2/Usuarios/forms.py
+++ ProyectoGAE4Galactic2/Usuarios/forms.py
@@ -1,9 +1,11 @@
 from typing import Any
+import re
 from django import forms
 #from django.contrib.auth.models import User
 from .models import User,Cliente
 #from core.models import Cliente
 from django.contrib.auth.models import Group
+from datetime import datetime
 
 class RegisterForm(forms.Form):
     username= forms.CharField(label= 'Nombre de usuario',required=True,
@@ -39,6 +41,32 @@ class RegisterForm(forms.Form):
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError('El nombre de usuario se encuentra en uso')
         return username
+    def clean_firstname(self):
+        first_name = self.cleaned_data.get('firstname')
+        if first_name:
+            for espacio in first_name.split():
+                if not espacio.isalpha():
+                    raise forms.ValidationError('El nombre solo debe contener caracteres alfabéticos.')
+        return first_name
+
+
+    def clean_lastname(self):
+        last_name = self.cleaned_data.get('lastname')
+      
+        if last_name:
+            for espacio in last_name.split():
+                if not espacio.isalpha():
+                    raise forms.ValidationError('El apellido solo debe contener caracteres alfabéticos.')
+        return last_name
+       
+    def clean_fechaNacimiento(self):
+        fecha_nacimiento = self.cleaned_data.get('fechaNacimiento')
+    
+    # Verificamos si el año de nacimiento es mayor a 2005
+        if fecha_nacimiento.year >= 2006:
+            raise forms.ValidationError('Debes ser mayor de edad para acceder a esta página.')
+    
+        return fecha_nacimiento
     
     def clean_email(self):
         email =self.cleaned_data.get('email')
@@ -46,6 +74,27 @@ class RegisterForm(forms.Form):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('Este correo ya se encuentra en uso')
         return email
+    
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+
+        # Verificar si la contraseña tiene al menos 6 caracteres
+        if len(password) < 6:
+            raise forms.ValidationError('La contraseña debe tener al menos 6 caracteres.')
+
+        # Verificar si la contraseña contiene al menos una letra
+        if not any(char.isalpha() for char in password):
+            raise forms.ValidationError('La contraseña debe contener al menos una letra.')
+
+        # Verificar si la contraseña contiene al menos un carácter especial
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            raise forms.ValidationError('La contraseña debe contener al menos un carácter especial.')
+
+        # Verificar si la contraseña contiene al menos un número
+        if not any(char.isdigit() for char in password):
+            raise forms.ValidationError('La contraseña debe contener al menos un número.')
+
+        return password
     
     def clean(self):
         cleaned_data= super().clean()
